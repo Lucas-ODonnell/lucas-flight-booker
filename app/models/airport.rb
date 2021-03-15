39ffe66be9ae4bef9dfc8c27b9b463b994d9bfc9
Lucas <lucas.odonnelll@gmail.com>
2021-03-15 16:50:34 -0700

    added flight airport relation

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -7,12 +7,14 @@
 
 
 class Airport < ApplicationRecord
-  #has_many :departing_flights,
-  #  class_name: "Flight",
-  #  foreign_key: "origin_id",
-  #  dependent: :destroy
-  #has_many :arriving_flights, 
-  #  class_name: "Flight",
-  #  foreign_key: "destination_id",
-  #  dependent: :destroy
+  has_many :departing_flights,
+    class_name: "Flight",
+    foreign_key: :origin_id,
+    dependent: :destroy,
+    inverse_of: :from_airport
+  has_many :arriving_flights, 
+    class_name: "Flight",
+    foreign_key: :destination_id,
+    dependent: :destroy,
+    inverse_of: :to_airport
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -10,9 +10,13 @@
 
 
 class Flight < ApplicationRecord
-  #belongs_to :origin,
-  #class_name: "Airport"
+  belongs_to :from_airport,
+  class_name: "Airport",
+  foreign_key: :origin_id,
+  inverse_of: :departing_flights
 
-  #belongs_to :destination,
-  #class_name: "Airport"
+  belongs_to :to_airport,
+  class_name: "Airport",
+  foreign_key: :destination_id,
+  inverse_of: :arriving_flights
 end
